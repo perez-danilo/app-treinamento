882b610733e97947459775a97a2e5a33ec224c5b
Danilo Perez <danilopz@gmail.com>
2020-01-30 20:22:04 -0300

    adicionado bloc

--- lib/pages/venda/venda.page.dart
+++ lib/pages/venda/venda.page.dart
@@ -18,23 +18,7 @@ class _VendaPageState extends State<VendaPage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          decoration: BoxDecoration(
-            // Box decoration takes a gradient
-            gradient: LinearGradient(
-              // Where the linear gradient begins and ends
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              // Add one stop for each color. Stops should increase from 0 to 1
-              stops: [0.1, 0.5, 0.7, 0.9],
-              colors: [
-                // Colors are easy thanks to Flutter's Colors class.
-                Colors.indigo[800],
-                Colors.indigo[700],
-                Colors.indigo[600],
-                Colors.indigo[400],
-              ],
-            ),
-          ),
+          color: Colors.white,
           child: Column(
             children: <Widget>[
               Expanded(
